ec966f616c9262a9809b7914d50dcbe0c26b4059
vamshichi <chvamshi03@gmail.com>
2024-08-03 17:13:52 +0530

    first commit

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import { Hono } from 'hono'
+
+const app = new Hono()
+
+app.post('/api/v1/user/signup', (c) => {
+  return c.text('Hello Hono!')
+})
+
+app.post('/api/v1/user/sihnin',(c)=>{
+  return c.text('vamshi')
+})
+
+app.post('/api/v1/blog', (c) => {
+  return c.text('Hello Hono!')
+})
+
+app.put('/api/v1/blog', (c) => {
+  return c.text('Hello Hono!')
+})
+
+app.get('/api/v1/blog/:id', (c) => {
+  return c.text('Hello Hono!')
+})
+
+app.get('/api/v1/blog/bulk', (c) => {
+  return c.text('Hello Hono!')
+})
+
+export default app
